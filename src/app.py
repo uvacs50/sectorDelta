51e1c71ef88ddfdee5a706388ca3dcf87cd6019c
Yessin <yessin@radouane.nl>
2023-11-26 02:11:05 +0100

    Partial template start

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import cufflinks as cf
 import datetime
 from flask import Flask, request, render_template
+import jinja_partials
 import plotly.graph_objects as go
 import cufflinks as cf
 import concurrent.futures
@@ -23,7 +24,7 @@ def load():
 
 
 app = Flask(__name__)
-
+jinja_partials.register_extensions(app)
 
 @app.route('/', methods=["GET"])
 def index():
@@ -196,3 +197,58 @@ def loading():
 
     return loading_state
 
+
+
+@app.route('/complex-basket2', methods=["GET", "POST"])
+def complex_basket2():
+    
+    if request.method == "GET":
+        # Create a sample DataFrame (replace this with your data)
+        # Default values or user-selected options from the form
+        default_basket_nominator = "AI"
+        default_basket_denominator = "L1"
+        default_interval = '1h'
+        default_bars_back = 200
+
+        # Initialize chart
+        chart = ComplexBasketChart(default_basket_nominator, default_basket_denominator, default_interval, default_bars_back)
+
+        # Get basket chart data
+        chart.get_complex_basket_data()
+        
+        # Get chart html
+        chart.get_chart_html()
+
+        return render_template('complex-basket2.html', chart_div=chart.html, selected_basket_nominator = chart.basket_nominator, 
+                               selected_basket_denominator = chart.basket_denominator, selected_bars_back = chart.bars_back)
+    
+    else:
+
+        # Create a sample DataFrame (replace this with your data)
+        # Default values or user-selected options from the form
+
+        t1 = int(time.time())*1000
+
+        default_basket_nominator = "AI"
+        default_basket_denominator = "L1"
+        default_interval = '1h'
+        default_bars_back = 200
+
+        # Get user-selected options from the form
+        selected_basket_nominator = request.form.get('basket_nominator', default_basket_nominator)
+        selected_basket_denominator = request.form.get('basket_denominator', default_basket_denominator)
+        selected_interval = request.form.get('interval', default_interval)
+        selected_bars_back = int(request.form.get('bars_back', default_bars_back))
+
+
+        chart = ComplexBasketChart(selected_basket_nominator, selected_basket_denominator, selected_interval, selected_bars_back)
+
+        # Get basket chart data
+        chart.get_complex_basket_data()
+        
+        # Get chart html
+        chart.get_chart_html()
+
+        return render_template('complex-basket2.html', chart_div=chart.html, selected_basket_nominator = chart.basket_nominator, 
+                               selected_basket_denominator = chart.basket_denominator, selected_bars_back = chart.bars_back)
+    
